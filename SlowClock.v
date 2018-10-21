b4e9f65f2d9e9c0af833938f042df13fc6e4bba2
zicog <zicog@example.com>
2018-10-21 17:36:55 +0300

    Switch to using xorshiro32++ run from 1.5 second clock

---
+++ SlowClock.v
@@ -0,0 +1,21 @@
+// Generator : SpinalHDL v1.1.5    git head : 0310b2489a097f2b9de5535e02192d9ddd2764ae
+// Date      : 21/10/2018, 12:39:04
+// Component : SlowClock
+
+
+module SlowClock (
+      output  io_Q,
+      input   clk,
+      input   reset);
+  reg [24:0] count;
+  assign io_Q = ((count & (25'b1000000000000000000000000)) == (25'b0000000000000000000000000));
+  always @ (posedge clk or posedge reset) begin
+    if (reset) begin
+      count <= (25'b0000000000000000000000000);
+    end else begin
+      count <= (count + (25'b0000000000000000000000001));
+    end
+  end
+
+endmodule
+
--- Top.v
+++ Top.v
@@ -2,7 +2,8 @@
 // What is this for ?!
 // `include "inc/timescale.vh"
 
-module Top (input clock_50, input[1:0] key, output[7:0] led);
+module Top (input clock_50, input[1:0] key, output[7:0] led, output P0, output P2);
+
 
     // Remove reset after 100 clocks
     reg reset = 1;
@@ -13,8 +14,8 @@ module Top (input clock_50, input[1:0] key, output[7:0] led);
         if (resetCount == 100) reset <= 0;
     end
 
-	 wire [31:0] prng;
-	 assign led[7:0] = prng[31:24];
+	 wire [15:0] prng;
+	 assign led[7:0] = prng[15:8];
 	 
     wire nextKey = !key[0];
 	 wire debouncedNextKey;
@@ -22,6 +23,27 @@ module Top (input clock_50, input[1:0] key, output[7:0] led);
 	 
 	 wire next;
 	 	 
+
+    wire sc;	 
+    wire scdb;	 
+    SlowClock slowClock (
+        .clk(clock_50),
+        .reset(reset),
+        .io_Q(sc)
+	 );
+	 assign P0 = sc;
+	 assign P2 = scdb;
+	 
+
+		 
+	 SwitchDebounce slowClockDebounce (
+        .clk(clock_50),
+        .reset(reset),
+        .io_D(sc),
+        .io_Q(scdb)
+    );
+
+	 
 	 // Debounce key 0 input into "next"
 	 SwitchDebounce switchDebounce (
         .clk(clock_50),
@@ -33,13 +55,13 @@ module Top (input clock_50, input[1:0] key, output[7:0] led);
 	 Monostable monostable (
         .clk(clock_50),
         .reset(reset),
-		  .io_trigger(debouncedNextKey),
+		  .io_trigger(scdb),
 		  .io_Q(next)
 	 );
 	 
 	 
 	 // Our Pseudo Random Number Generator
-    Xoroshiro64PlusPlus xoroshiro64PlusPlus (
+    Xoroshiro32PlusPlus xoroshiro32PlusPlus (
         .clk(clock_50),
         .reset(reset),
 		  .io_next(next),
