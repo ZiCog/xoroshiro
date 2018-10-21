4d90cd10eabb8fe6974b3bcfebe8ff9bcae83fbb
zicog <zicog@example.com>
2018-10-21 17:34:53 +0300

    Added testbench for xorshiro64++

--- src/main/scala/XoroshiroPlusPlus/xoroshiro-plusplus-sim.scala
+++ src/main/scala/XoroshiroPlusPlus/xoroshiro-plusplus-sim.scala
@@ -11,12 +11,12 @@ import scala.util.Random
 object XoroshiroPlusPlusSim {
   def main(args: Array[String]) {
 
-    val compiled = SimConfig.withWave.compile{
+    val compiled64 = SimConfig.withWave.compile{
       val dut = new Xoroshiro64PlusPlus ;
       dut
     }
 
-    compiled.doSim("test_Xoroshiro64PlusPlus"){dut =>
+    compiled64.doSim("test_Xoroshiro64PlusPlus"){dut =>
       // Fork a process to generate the reset and the clock on the dut
       dut.clockDomain.forkStimulus(period = 10)
 
@@ -45,5 +45,43 @@ object XoroshiroPlusPlusSim {
         idx += 1
       }
     }
+
+    val compiled32 = SimConfig.withWave.compile{
+      val dut = new Xoroshiro32PlusPlus ;
+      dut
+    }
+
+    compiled32.doSim("test_Xoroshiro32PlusPlus"){dut =>
+      // Fork a process to generate the reset and the clock on the dut
+      dut.clockDomain.forkStimulus(period = 10)
+
+      var modelState = 0
+
+      var idx = 0
+      while(idx < 16){
+        // Drive the DUT inputs with random values
+        dut.io.next #= true
+
+        // Wait a rising edge on the clock
+        dut.clockDomain.waitRisingEdge()
+
+        println(dut.io.prng.toLong.toHexString.padTo(4, "0").mkString)
+
+        // Check that the DUT values match with the reference model ones
+        // val modelFlag = modelState == 0 || dut.io.cond1.toBoolean
+        // assert(dut.io.state.toInt == modelState)
+        // assert(dut.io.flag.toBoolean == modelFlag)
+
+        // Update the reference model value
+        //if(dut.io.cond0.toBoolean) {
+        //  modelState = (modelState + 1) & 0xFF
+        //}
+
+        idx += 1
+      }
+    }
+
+
+
   }
 }
