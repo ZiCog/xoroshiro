f2e72b2ea08b253d02bfc738c46eb8c2b4ff08b3
zicog <zicog@example.com>
2018-10-22 22:27:06 +0300

    Sync *.v files

--- SlowClock.v
+++ SlowClock.v
@@ -1,5 +1,5 @@
 // Generator : SpinalHDL v1.1.5    git head : 0310b2489a097f2b9de5535e02192d9ddd2764ae
-// Date      : 22/10/2018, 22:03:19
+// Date      : 22/10/2018, 22:08:21
 // Component : SlowClock
 
 
