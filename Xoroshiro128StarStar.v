e115f5f545a24a9331f1ee35e46ba63789b2023a
zicog <zicog@example.com>
2018-11-17 22:41:20 +0200

    AsyncReceiver test cleaned up, now pushes 256 bytes through.

--- Xoroshiro128StarStar.v
+++ Xoroshiro128StarStar.v
@@ -1,5 +1,5 @@
 // Generator : SpinalHDL v1.1.5    git head : 0310b2489a097f2b9de5535e02192d9ddd2764ae
-// Date      : 15/11/2018, 23:21:59
+// Date      : 17/11/2018, 19:47:16
 // Component : Xoroshiro128StarStar
 
 
--- src/main/scala/xoroshiro/AsyncReceiver_sim.scala
+++ src/main/scala/xoroshiro/AsyncReceiver_sim.scala
@@ -17,12 +17,6 @@ object AsyncReceiverSim {
       dut.bitCount.simPublic()
       dut.shifter.simPublic()
       dut.bitTimer.simPublic()
-      /*
-      dut.fifo.head.simPublic()
-      dut.fifo.tail.simPublic()
-      dut.fifo.full.simPublic()
-      dut.fifo.empty.simPublic()
-      */
       dut
     }
     compiled.doSim("AsyncReceiver") { dut =>
@@ -45,7 +39,7 @@ object AsyncReceiverSim {
         }
       }
 
-      def initDutSignals (): Unit = {
+      def initDutSignals(): Unit = {
         dut.io.enable #= false
         dut.io.mem_valid #= false
         dut.io.mem_addr #= 0
@@ -65,7 +59,7 @@ object AsyncReceiverSim {
         println()
       }
 
-      def busRead(address:Int): Unit@suspendable = {
+      def busRead(address: Int): Unit@suspendable = {
         dut.io.enable #= true
         dut.io.mem_valid #= true
         dut.io.mem_addr #= address
@@ -95,72 +89,40 @@ object AsyncReceiverSim {
         }
       }
 
+      def generateSerialByte(byte: Int): Unit@suspendable = {
+        // Start bit
+        dut.io.rx #= false
+        waitOneBitTime()
+
+        var bitPos = 0
+        while (bitPos < 8) {
+          // Bit 0
+          dut.io.rx #= (byte >> bitPos & 0x01) == 1
+          waitOneBitTime()
+          bitPos += 1
+        }
+
+        // Stop bit
+        dut.io.rx #= true
+        waitOneBitTime()
+      }
+
       // Fork a process to generate the reset and the clock on the dut
       dut.clockDomain.forkStimulus(period = 10)
 
       initDutSignals()
+      var loops = 0
+      while (loops < 256) {
 
-      var idx = 0
-      while (idx < 40) {
-        //println("Reading RX status, expect 0")
-        busRead(4)
+        generateSerialByte(loops)
 
-        // Start bit
-          dut.io.rx #= false
-          waitOneBitTime()
-        // Bit 0
-          dut.io.rx #= false
-          waitOneBitTime()
-        // Bit 1
-          dut.io.rx #= true
-          waitOneBitTime()
-        // Bit 2
-          dut.io.rx #= false
-          waitOneBitTime()
-        // Bit 3
-          dut.io.rx #= true
-          waitOneBitTime()
-        // Bit 4
-          dut.io.rx #= false
-          waitOneBitTime()
-        // Bit 5
-          dut.io.rx #= true
-          waitOneBitTime()
-        // Bit 6
-          dut.io.rx #= false
-          waitOneBitTime()
-        // Bit 7
-          dut.io.rx #= true
-          waitOneBitTime()
-        // Stop bit
-          dut.io.rx #= true
-          waitOneBitTime()
-
-        idx += 1
-      }
-
-      idx = 0
-      while (idx < 65) {
-
-        println("Reading RX , expect aa")
+        println(f"Reading RX data, expect ${loops}%02d: ")
         busRead(0)
-        /*
-        print(f"empty: ${dut.fifo.empty.toBoolean}, ")
-        print(f"full: ${dut.fifo.full.toBoolean}, ")
-        print(f"head: ${dut.fifo.head.toInt}%08d, ")
-        print(f"tail: ${dut.fifo.tail.toInt}%08d, ")
-        println()
-        */
-        println("Reading RX status, expect 0")
+
+        println("Reading RX status, expect 0: ")
         busRead(4)
-        /*
-        print(f"empty: ${dut.fifo.empty.toBoolean}, ")
-        print(f"full: ${dut.fifo.full.toBoolean}, ")
-        print(f"head: ${dut.fifo.head.toInt}%08d, ")
-        print(f"tail: ${dut.fifo.tail.toInt}%08d, ")
-        println()
-        */
-        idx += 1
+
+        loops += 1
       }
     }
   }
