c545c3dda299dbf9c7ec0245fb1b345820d95987
zicog <zicog@example.com>
2018-12-02 10:30:05 +0200

    Removed rxSync2, use rxSync1

--- src/main/scala/xoroshiro/AsyncReceiver.scala
+++ src/main/scala/xoroshiro/AsyncReceiver.scala
@@ -31,10 +31,10 @@ class AsyncReceiver extends Component {
   baudClockX64Sync2 := baudClockX64Sync1
 
   val  rxSync1 = Reg(Bool) init False
-  val  rxSync2 = Reg(Bool) init False
+  //val  rxSync2 = Reg(Bool) init False
 
   rxSync1 := io.rx
-  rxSync2 := rxSync1
+  //rxSync2 := rxSync1
 
   val S0 = 0
   val S1 = 1
@@ -51,7 +51,7 @@ class AsyncReceiver extends Component {
     is(S0) {
       state := S0
       // Waiting for falling edge of start bit
-      when(rxSync2.fall) {
+      when(rxSync1.fall) {
         state := S1
         bitTimer := 31
       }
@@ -60,7 +60,7 @@ class AsyncReceiver extends Component {
       state := S1
       // Check valid start bit
       when(bitTimer === 0) {
-        when(rxSync2 === False) {
+        when(rxSync1 === False) {
           bitTimer := 63
           state := S2
         } otherwise {
@@ -73,7 +73,7 @@ class AsyncReceiver extends Component {
       when (baudClockX64Sync2.rise) {
         // Clock in data bits
         when (bitTimer === 0) {
-          shifter(bitCount) := rxSync2
+          shifter(bitCount) := rxSync1
           bitCount := bitCount + 1
           when(bitCount === 7) {
             state := S3
@@ -85,7 +85,7 @@ class AsyncReceiver extends Component {
       state := S3
       // Check stop bit
       when(bitTimer === 0) {
-        when(rxSync2 === True) {
+        when(rxSync1 === True) {
           when(!fifo.io.full) {
             fifo.io.write := True
           }
